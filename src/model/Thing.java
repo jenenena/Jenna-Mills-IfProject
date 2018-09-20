a8e2c683e0ac13e07f3ca509fc7c476a886964a1
Jenna Mills <superangelfish555@gmail.com>
2018-09-20 08:59:01 -0600

    Added in some java docs and Worked on the model.

--- src/model/Thing.java
+++ src/model/Thing.java
@@ -3,18 +3,57 @@ package model;
 public class Thing
 {
 
-	//data members go up here in the model, they're always private
+	//data members go up here in the model, they're always private.
+	//data members aren't like variables, they get a default value! Variables do not.
+	//all numbers --> 0
+	//booleans --> false
+	//objects --> null
 	private String name;
 	private boolean isSad;
-	private int flowers;
+	private boolean flowers;
 	
-	public Thing(String name, boolean isSad, int flowers)
+	
+	
+
+	/**
+	 * Default constructor for when I don't yet know the information :)
+	 * Here I initialize data members with "bad" values to be changed.
+	 */
+	public Thing()
+	{
+		this.name = "unnamed buddy";
+		this.isSad = false;
+		this.flowers = false;
+	}
+	
+	
+	
+	
+	/**
+	 * better constructor when I know information for the Thing object.
+	 * @param name is the name he'll be given
+	 */
+
+	public void setName(String name)
 	{
 		this.name = name;
+	}
+	
+	public void setIsSad(boolean isSad)
+	{
 		this.isSad = isSad;
-		this.flowers = flowers;
 	}
 
+	public void setFlowers(boolean flowers)
+	{
+		this.flowers = flowers;
+	}
+	
+	
+	
+
+	
+	//getters!
 	public String getName()
 	{
 		return name;
@@ -25,7 +64,7 @@ public class Thing
 		return isSad;
 	}
 	
-	public int getFlowers()
+	public boolean getFlowers()
 	{
 		return flowers;
 	}
