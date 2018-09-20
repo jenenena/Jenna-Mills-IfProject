33b6c876cabaa6edea31a9e2d94f2a1b8aa1c449
Jenna Mills <superangelfish555@gmail.com>
2018-09-20 08:27:27 -0600

    Built project. Defined data members.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,22 @@
 package controller;
 
+//import section
+import javax.swing.JOptionPane;
+import model.Thing;
+
 public class Controller
 {
-
+	private Thing myThing;
+	
+	public Controller()
+	{
+		myThing = new Thing();
+	}
+	
+	public void start()
+	{
+		String myThingName = JOptionPane.showInputDialog(null, "What should we call my thing?");
+		
+		JOptionPane.showMessageDialog(null, "This is my Thing. His name is: " + myThing.getName());
+	}
 }
--- src/model/Thing.java
+++ src/model/Thing.java
@@ -5,9 +5,28 @@ public class Thing
 
 	//data members go up here in the model, they're always private
 	private String name;
+	private boolean isSad;
+	private int flowers;
 	
-	public Thing(String name)
+	public Thing(String name, boolean isSad, int flowers)
 	{
 		this.name = name;
+		this.isSad = isSad;
+		this.flowers = flowers;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean getIsSad()
+	{
+		return isSad;
+	}
+	
+	public int getFlowers()
+	{
+		return flowers;
 	}
 }
