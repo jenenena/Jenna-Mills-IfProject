7227f52b445ad410fa05347e9b0d5e140b7862b0
Jenna Mills <superangelfish555@gmail.com>
2018-09-20 07:45:23 -0600

    Initial commit

---
+++ src/model/Thing.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Thing
+{
+
+	//data members go up here in the model, they're always private
+	private String name;
+	
+	public Thing(String name)
+	{
+		this.name = name;
+	}
+}
